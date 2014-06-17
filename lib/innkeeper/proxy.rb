b310e0c13dc94b208e0b46e0b8bfef80e5f63ff6
Mike Goggin <mgoggin@dvauction.com>
2014-06-17 10:29:42 -0500

    Added `ipaddress` dep and condition on failure. Proxy uses `ipaddress` to check if the host is an IP. You can control how Innkeeper will fail when `check_in` does not return anything.
    `config.redirect_on_failure` will accept one of the following:
    * `:host_domain` -- Will redirect to the host domain (e.g. test.example.com fails to example.com)
    * `:host_domain_with_www` -- Will redirect to the host domain with www in front of it (e.g. test.example.com fails to www.example.com)
    * `:failure_app` -- With call the failure app defined in `config.failure_app`
    * An instance of `String` containing the URL to which you want to redirect
    
    If `config.failure_app` is not defined then Innkeeper will fail silently
    by calling the next app in the middleware stack.
    
    If using a `String` instance in `config.redirect_on_failure`, the `String`
    is passed to rack unmodified, so make sure that it is properly formatted.

--- lib/innkeeper/proxy.rb
+++ lib/innkeeper/proxy.rb
@@ -1,8 +1,11 @@
+require 'ipaddress'
+
 module Innkeeper
 	class Proxy
 		include ::Innkeeper::Rack::Mixin
 		extend ::Forwardable
 
+		attr_reader :domain, :host_domain, :subdomain
 		attr_accessor :manager
 
 		def_delegators :manager, :env, :config
@@ -11,8 +14,19 @@ module Innkeeper
 			@manager = manager
 			@halted = false
 
+			unless IPAddress.valid?(request.host)
+				domain_parts = request.host.split('.')
+
+				if domain_parts.size > 2
+					@subdomain = domain_parts.shift
+				end
+
+				@host_domain = domain_parts.join('.')
+				@domain = request.host
+			end
+
 			if respond_to?(:find_by_environment)
-				_tenant = send(:find_by_environment, request)
+				_tenant = send(:find_by_environment, subdomain, host_domain, request)
 				halt! unless _tenant
 				send("tenant=", _tenant)
 			end
@@ -41,15 +55,29 @@ module Innkeeper
 
 		def fail!
 			halt!
-			if config.require_infer && config.failure_app.respond_to?(:call)
-				config.failure_app.call(request)
-			else
-				manager.app.call(env)
+
+			if config.require_infer && config.redirect_on_failure
+				if config.redirect_on_failure == :host_domain
+					return redirect! "//#{host_domain}"
+				elsif config.redirect_on_failure == :host_domain_with_www
+					return redirect! "//www.#{host_domain}"
+				elsif config.redirect_on_failure == :failure_app && config.failure_app.respond_to?(:call)
+					return config.failure_app.call(request)
+				elsif config.redirect_on_failure.is_a? String
+					return redirect! "#{config.redirect_on_failure}"
+				end	
 			end
+
+			manager.app.call(env)
 		end
 
 		def halt!
 			@halted = true
+		end
+
+		private
+		def redirect!(uri, code = 302)
+			[code, { 'Location' => "#{uri}" }, []]
 		end
 	end
 end
